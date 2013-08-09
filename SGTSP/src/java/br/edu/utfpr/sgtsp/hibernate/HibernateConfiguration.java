b875b630feac03704ff7a0adf67f8ed9ddae9e57
tassinha <tanaka.tassia@gmail.com>
2013-08-09 20:35:30 -0300

    Arquivos de configuração do projeto.

---
+++ SGTSP/src/java/br/edu/utfpr/sgtsp/hibernate/HibernateConfiguration.java
@@ -0,0 +1,95 @@
+package br.edu.utfpr.sgtsp.hibernate;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class HibernateConfiguration {
+
+    private static String userName = "root";
+    private static String password = "root";
+    private static String dataBase = "sgtsp";
+    private static String host = "localhost:3306";
+    private static AnnotationConfiguration cfg = null;
+    private static SessionFactory factory = null;
+    
+    public static Session openConnect() {
+        if (cfg == null) {
+            cfg = new AnnotationConfiguration();
+            cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            cfg.setProperty("hibernate.connection.username", userName);
+            cfg.setProperty("hibernate.connection.password", password);
+            cfg.setProperty("hibernate.connection.url", "jdbc:mysql://" + host + "/" + dataBase);
+            cfg.setProperty("hibernate.show_sql", "false");
+            cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+            cfg.setProperty("hibernate.connection.autocommit", "true");
+
+            factory = cfg.buildSessionFactory();
+        }
+        return factory.openSession();
+    }
+    
+    
+    public static AnnotationConfiguration getCfg() {
+        return cfg;
+    }
+
+    public static void setCfg(AnnotationConfiguration cfg) {
+        HibernateConfiguration.cfg = cfg;
+    }
+
+    public static String getDataBase() {
+        return dataBase;
+    }
+
+    public static void setDataBase(String dataBase) {
+        HibernateConfiguration.dataBase = dataBase;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        HibernateConfiguration.password = password;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setUserName(String userName) {
+        HibernateConfiguration.userName = userName;
+    }
+
+    public static String getHost() {
+        return host;
+    }
+
+    public static void setHost(String host) {
+        HibernateConfiguration.host = host;
+    }
+
+    public static void criarSchema() {
+        openConnect().close();
+        org.hibernate.tool.hbm2ddl.SchemaExport schemaEx = new SchemaExport(cfg);
+        schemaEx.create(true, true);
+    }
+
+    public static List<Object> runHQLQuery(String hql) {
+        Session session = TransactionManager.getCurrentSession();
+        return session.createQuery(hql).list();
+    }
+
+    public static ArrayList<Class> getEntityClasses() {
+        ArrayList<Class> classes = new ArrayList<Class>();
+        if (cfg == null) {
+            openConnect();
+        }
+        Iterator i = cfg.getClassMappings();
+        while (i.hasNext()) {
+            classes.add(((PersistentClass) i.next()).getMappedClass());
+        }
+        return classes;
+    }
+}
